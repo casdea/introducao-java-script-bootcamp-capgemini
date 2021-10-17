5ecb48f5f75718c5cbd851e38197008b6e67634a
casdea <casdea@gmail.com>
2021-10-16 23:42:13 -0300

    Outros testes

--- js/main.js
+++ js/main.js
@@ -23,3 +23,41 @@ console.log(lista.toString()[2])
 console.log(lista.join("/"));
 var fruta = {nome: "maça", cor: "vermelha"}
 console.log(fruta)
+
+var idade3 = 18;
+
+if (idade > 18) {
+//    alert("Maior de idade");
+    
+}
+else {
+//    alert("menor de idade")
+}
+
+//var idade4 = prompt("Qual sua idade ?")
+
+//if (idade>=18) {
+ //   alert("Maior de idade");    
+//}
+//else {
+//    alert("Menor de Idade")
+//}
+
+var count = 0;
+while (count < 5) {
+    console.log(count);
+    //count =count+1;
+    count++;
+}
+
+for (let index = 0; index < lista.length; index++) {
+    const element = lista[index];
+    console.log("elemento "+element)
+}
+
+var data = new Date();
+console.log(data.getDate());
+console.log(data.getMonth()+1);
+console.log(data.getFullYear());
+
+console.log("teste "+" teste 2 " + 1425+"-"+1519)
