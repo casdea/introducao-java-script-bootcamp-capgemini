54f6d1475d11e575d064adbf621722962a16fefa
casdea <casdea@gmail.com>
2021-10-16 22:19:08 -0300

    Introducao ao Java Script

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var nome = "Carlos Andrade";
+var idade = 50;
+var idade2 = 10;
+
+//alert(nome + " tem "+idade+" anos");
+//alert(idade+idade2);
+
+console.log(idade + idade2)
+var frase = "Japao melhor time do mundo";
+console.log(frase.replace("Japao","Brasil"))
+console.log(frase.toUpperCase())
+
+var lista = ["maca", "pera", "laranja"];
+console.log(lista[2])
+lista.push("uva")
+console.log(lista)
+lista.pop()
+console.log(lista)
+console.log(lista.length)
+console.log(lista.reverse())
+console.log(lista.reverse().toString())
+console.log(lista.toString()[2])
+console.log(lista.join("/"));
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta)
